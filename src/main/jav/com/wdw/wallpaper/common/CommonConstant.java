26cf23ddda73b4850a75c72b9a6557a6795a7b8b
DavidDevper <125234471+DavidDevper@users.noreply.github.com>
2023-08-08 15:34:18 +0800

    commit constant

---
+++ src/main/jav/com/wdw/wallpaper/common/CommonConstant.java
@@ -0,0 +1,9 @@
+package com.wdw.wallpaper.common;
+
+public class CommonConstant {
+
+    public static final String MYSQL_URL = "jdbc:mysql://172.25.14.106:3306/wallpaper?user=root&password=Wdw878403!&autoReconnect=true&serverTimezone=UTC&allowMultiQueries=true&useSSL=false";
+
+    public static final String SCHEMA_URL = "jdbc:mysql://172.25.14.106:3306/information_schema?user=root&password=Wdw878403!&autoReconnect=true&serverTimezone=UTC&allowMultiQueries=true&useSSL=false";
+
+}
